dbd61668c1f8b0c218ffe4586e506305028dc7dc
Harun <mdharunbd88@gmail.com>
2022-11-25 10:14:31 +0600

    Header design update

--- src/pages/home-page/HomePage.js
+++ src/pages/home-page/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from "../../components/header/Header";
 import HeroSection from "../../components/hero-section/HeroSection";
+import About from "../../components/about/About";
 const HomePage = () => {
     return (
         <div className="home-page">
             <div className="main-head">
                 <Header />
                 <HeroSection/>
+                <About/>
             </div>
         </div>
     );
